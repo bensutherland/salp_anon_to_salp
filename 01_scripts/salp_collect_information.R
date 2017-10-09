213f1a94f2b33999fa26b36a658d5d866b3e76bf
bensutherland <bensutherland7@gmail.com>
2017-10-09 15:39:47 -0700

    Add toggle for male/female and remove unecessary sections from collection script

--- 01_scripts/salp_collect_information.R
+++ 01_scripts/salp_collect_information.R
@@ -1,23 +1,20 @@
 # R script to clean up the input files to be used in MapComp (iterative)
+# rm(list=ls())
+
 setwd("~/Documents/bernatchez/01_Sfon_projects/14_JS_migration/salp_anon_to_salp")
 
-# Choose female or male map
+# Choose which map to use from female and male
+sex <- "female"
+#sex <- "male"
 
-# used to combine the following files:
-#female markers 
-female.marker.file <- "02_data/salp_female_map.csv" # markers
+# Create name for marker file
+marker.file <- paste(c("02_data/salp_", sex, "_map.csv"), sep = "", collapse = "")
 
-#male markers
-male.marker.file <- "02_data/salp_male_map.csv"
-
-# seq file
-seq.file <- "02_data/salp_marker_and_seq.csv" # seq
-
-marker.file <- female.marker.file
-marker.file <- male.marker.file
+# Name seq file
+seq.file <- "02_data/salp_marker_and_seq.csv"
 
 
-# import seq file
+# Import files
 seq <- read.csv(file = seq.file, header = F, col.names = c("mname","seq"))
 head(seq)
 str(seq)
@@ -28,27 +25,12 @@ head(markers)
 str(markers)
 
 # Match marker names between the two files:
-length(intersect(markers$mname, seq$mname)) #1656 matching
+length(intersect(markers$mname, seq$mname)) #1656 matching (for female)
 
-# Check your LG names
+# Check your LG names, note the two LGs that need to be combined into one (currently split into arms)
 levels(markers$LG)
 
-# fix the LG issues (AC20 and AC4) in the markers file
-# find all rows with AC20b
-markers[which(markers$LG == "AC20a"),] 
-max(markers[which(markers$LG == "AC20a"),3])
-
-### THIS SECTION IS EXTRA ###
-add.first.arm.value <- max(markers[which(markers$LG == "AC20a"),3])
-
-# total length of AC20a is add.first.arm.value (cM), so add this to AC20b
-
-which(markers$LG == "AC20b")
-markers[which(markers$LG == "AC20b"),3] + add.first.arm.value
-
-### END THIS SECTION IS EXTRA ###
-
-# adds the first part of LG to the second (cumulative)
+# Add the first part of LG to the second (cumulative)
 # AC20
 add.cumul <- max(markers[which(markers$LG == "AC20a"),3]) # number to add
 markers[which(markers$LG == "AC20b"),3]
@@ -69,6 +51,7 @@ markers[which(markers$LG == "AC4q"),3]
 markers$LG <- gsub(pattern = "4p", replacement = "4", x = markers$LG)
 markers$LG <- gsub(pattern = "4q", replacement = "4", x = markers$LG)
 unique(markers$LG)
+
 
 ## Change LG to numeric
 markers$LG <- gsub("[[:punct:]]", "", markers$LG) #remove plus sign
@@ -100,14 +83,10 @@ head(salp)
 salp <- salp[order(salp$LG, salp$pos),]
 head(salp)
 
+# Create output filename
+output.filename <- paste(c("02_data/salp_", sex, "_merged_sorted_clean.csv"), sep = "", collapse = "")
+
 # Write out to use in MapComp iterative
-
-# Female save out
-write.table(salp, file = "02_data/salp_male_merged_sorted_clean.csv"
+write.table(salp, file = output.filename
             , row.names = F, col.names = F, sep = ","
             , quote = F)
-# Male save out
-write.table(salp, file = "02_data/salp_male_merged_sorted_clean.csv"
-            , row.names = F, col.names = F, sep = ","
-            , quote = F)
-
