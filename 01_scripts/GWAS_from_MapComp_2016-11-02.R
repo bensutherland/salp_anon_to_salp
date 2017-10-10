fa054d01dc2f36fef98d9b9bff78657a8f0ac0b7
bensutherland <bensutherland7@gmail.com>
2017-10-09 21:38:52 -0700

    Remove unused code from gwas plot script

--- 01_scripts/GWAS_from_MapComp_2016-11-02.R
+++ 01_scripts/GWAS_from_MapComp_2016-11-02.R
@@ -124,32 +124,6 @@ right.grey <- c(cumul.leng[c(TRUE, FALSE)])   # find x-axis positions, right sid
 # Find position for labels
 position <- ((right.grey[1:18] - left.grey[1:18])/2) + left.grey[1:18]
 
-# #### Plot SINGLE FST ####
-# # Plot an empty plot
-# #par(mfrow=c(1,1), mar= c(2,3,0.5,1) + 0.2, mgp = c(2,0.75,0))
-# plot(x = c(0, max(cumul.leng)), y = c(0, max(gwas.fst$fst+0.025)), type = "n"
-#      , xaxt = 'n'
-#      , xlab = "Arctic Char Linkage Group", ylab = "Fst",
-#      las = 1, cex.axis = 0.8)
-# # Add grey boxes for LGs
-# rect(xleft = left.grey[0:18], xright= right.grey[0:19],
-#      ybottom = 0, ytop = 11,
-#      col = c("lightgrey"),
-#      border=NA)
-# 
-# # Find position for labels
-# position <- ((right.grey[1:18] - left.grey[1:18])/2) + left.grey[1:18]
-# 
-# # Add axis and points
-# labels.no21 <- c(seq(from = 1, to = 20), seq(from = 22, to = 37))
-# labels.to.add <- labels.no21[c(TRUE, FALSE)]
-# axis(side = 1, at = position, labels = labels.to.add
-#      , cex.axis = 0.8 )
-# points(gwas.fst$totpos, gwas.fst$fst, type = "p", cex = 0.8)
-# 
-# # save as 10 x 6
-
-
 
 ##### Plot the three test statistics ####
 # Plot an empty plot
@@ -258,45 +232,3 @@ gwas.fst$mname[which(
 # 
 # gwas.fst$totpos[gwas.fst$mname==298178] #find out where a marker is
 
-
-
-
-
-
-
-###### OLD MATERIAL #####
-# ##### OBTAIN ONLY SIGNIFICANT FST MNAMES ####
-# outliers94 <- read.table(file = "../00_resources/94snps-outliers_2016-11-11.txt", header = T)
-# head(outliers94)
-# names(outliers94) #"SNP" is the mname
-# 
-# outliers94$SNP
-# significantSNPs <- outliers94$SNP
-# 
-# gwas.fst.sig <- gwas.fst[gwas.fst$mname %in% significantSNPs, ] # select only the significant snps
-# length(gwas.fst.sig$mname) # how many outliers were present in the positioned markers?
-# 
-# points(gwas.fst.sig$totpos, gwas.fst.sig$fst, pch = 3, cex = 1)
-# 
-# 
-# # export markers with positions
-# write.csv(gwas.fst$mname, file = "salp_markers_with_Sfon_positions_2016-11-13.csv")
-
-# gwas.fst$mname[which(gwas.fst$BS.fst)]
-
-# ##### DATA EXPLORATION #####
-# # More detail on markers of interest
-# gwas.fst$totpos[which(gwas.fst$fst > 0.03)] # positions
-# gwas.fst$mname[which(gwas.fst$fst > 0.03)] # marker names
-# 
-# # find markers in BC38 with Fst > 0.1
-# chr.end.df
-# chr.start <- 1721.3238
-# chr.end <- 1977.5399
-# gwas.fst$mname[which(gwas.fst$fst > 0.005 & 
-#                        gwas.fst$totpos > chr.start & 
-#                        gwas.fst$totpos < chr.end)]
-# 
-# gwas.fst.sig$mname[which(gwas.fst.sig$totpos > chr.start & gwas.fst.sig$totpos < chr.end)]
-# 
-# gwas.fst$totpos[gwas.fst$mname==298178] #find out where a marker is
